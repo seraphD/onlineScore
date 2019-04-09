cc818d2a03d5996563680bd99926762971972eca
seraphD <36287146+seraphD@users.noreply.github.com>
2019-04-09 20:57:47 +0800

    Update mysql.js

--- scoreS/routes/db/mysql.js
+++ scoreS/routes/db/mysql.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  user     : 'root',
-  password : '123',
-  database : 'online_scoe'
+  user     : 'score',
+  password : 'Wingofyou123.',
+  database : 'online_score'
 });
 
 /* 
